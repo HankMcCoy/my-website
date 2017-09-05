eb53d0388057624bac82e7149fcadba89f0339a2
HankMcCoy <thomas@blossor.com>
2017-09-05 00:38:45 -0700

    Make it kind of mobile friendly

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,78 +2,172 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
-import { Div, H2, Img, Ul, Li } from "glamorous";
+import glamorous, { Div, H2, Img, Ul, Li } from "glamorous";
 
 import Spacer from "../components/spacer";
 import myFace from "../imgs/my-face.jpg";
 import "./index.css";
 
+const MOBILE_WIDTH = 1000;
+
+const SideBarRoot = glamorous.div({
+  color: "#fff",
+  backgroundColor: "#484C4C",
+  display: "flex",
+  borderStyle: "solid",
+  borderColor: "#333",
+  [`@media(min-width: ${MOBILE_WIDTH}px)`]: {
+    height: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "40px",
+    borderWidth: "0 0 0 2px"
+  },
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    height: "50px",
+    borderWidth: "0 0 2px 0",
+    padding: "0 30px"
+  },
+  "@media(max-width: 625px)": {
+    padding: "0 15px 0 15px"
+  }
+});
+
+const AvatarImg = glamorous.img({
+  borderRadius: "50%",
+  boxShadow: "0 2px 2px rgba(0, 0, 0, 0.5)",
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    display: "none"
+  }
+});
+
+const Heading = glamorous.div({
+  margin: 0,
+  fontFamily: "Georgia, serif",
+  fontWeight: 400,
+  fontSize: "36px",
+  fontStyle: "italic",
+  position: "relative",
+  "& a": {
+    color: "#fff",
+    textDecoration: "none"
+  },
+  [`@media(min-width: ${MOBILE_WIDTH}px)`]: {
+    marginTop: "20px",
+    marginBottom: "40px"
+  },
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    lineHeight: "50px"
+  },
+  "@media(max-width: 625px)": {
+    "& a": {
+      color: "transparent"
+    },
+    "& a::after": {
+      content: "TB",
+      color: "#fff",
+      position: "absolute",
+      left: 0,
+      top: 0
+    }
+  }
+});
+
+const LinkList = glamorous.ul({
+  listStyle: "none",
+  padding: 0,
+  margin: 0,
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+  }
+});
+
+const SideBarListItem = glamorous.li({
+  [`@media(min-width: ${MOBILE_WIDTH}px)`]: {
+    fontSize: "24px",
+    marginBottom: "20px"
+  },
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    fontSize: "18px",
+    ":not(:first-child)": {
+      marginLeft: "20px"
+    }
+  }
+});
+
 const SideBarLink = ({ children, to }) => {
   return (
-    <Li>
+    <SideBarListItem>
       <Link
         to={to}
         style={{
           color: "#eee",
-          fontSize: "24px",
           fontWeight: "300",
           textDecoration: "none"
         }}
       >
         {children}
       </Link>
-    </Li>
+    </SideBarListItem>
   );
 };
+
+const Aligner = glamorous.div({
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%"
+  }
+});
+
 const SideBar = () => (
-  <Div
-    color="#fff"
-    backgroundColor="#484C4C"
-    height="100%"
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-  >
-    <Spacer height={40} />
-    <Img
-      src={myFace}
-      borderRadius="50%"
-      boxShadow="0 2px 2px rgba(0, 0, 0, 0.5)"
-    />
-    <Spacer height={20} />
-    <Div>
-      <H2
-        margin={0}
-        fontFamily="Georgia, serif"
-        fontWeight={400}
-        fontSize="36px"
-        fontStyle="italic"
-      >
-        <Link
-          to="/"
-          style={{
-            color: "#fff",
-            textDecoration: "none"
-          }}
-        >
-          Thomas Beirne
-        </Link>
-      </H2>
-      <Spacer height={40} />
-      <Ul listStyle="none" padding={0} margin={0}>
+  <SideBarRoot>
+    <AvatarImg src={myFace} />
+    <Aligner>
+      <Heading>
+        <Link to="/">Thomas Beirne</Link>
+      </Heading>
+      <LinkList>
         <SideBarLink to="/about">About</SideBarLink>
-        <Spacer height={20} />
         <SideBarLink to="/">Posts</SideBarLink>
-        <Spacer height={20} />
         <SideBarLink to="/resume">Résumé</SideBarLink>
-        <Spacer height={20} />
         <SideBarLink to="/training">Training</SideBarLink>
-      </Ul>
-    </Div>
-  </Div>
+      </LinkList>
+    </Aligner>
+  </SideBarRoot>
 );
 
-const TemplateWrapper = ({ children }) => (
+const ContentWrapper = glamorous.div({
+  [`@media(min-width: ${MOBILE_WIDTH}px)`]: {
+    padding: "40px 60px",
+    marginRight: "380px"
+  },
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    padding: "40px 30px",
+    marginTop: "50px"
+  },
+  "@media(max-width: 625px)": {
+    padding: "40px 15px"
+  }
+});
+
+const SideBarWrapper = glamorous.div({
+  position: "fixed",
+  top: 0,
+  right: 0,
+  [`@media(min-width: ${MOBILE_WIDTH}px)`]: {
+    bottom: 0,
+    width: "380px"
+  },
+  [`@media(max-width: ${MOBILE_WIDTH - 1}px)`]: {
+    left: 0,
+    height: "50px"
+  }
+});
+
+const Layout = ({ children }) => (
   <Div display="flex" minHeight="100%">
     <Helmet
       title="Thomas Beirne"
@@ -82,17 +176,11 @@ const TemplateWrapper = ({ children }) => (
         { name: "keywords", content: "sample, something" }
       ]}
     />
-    <Div marginRight="380px" padding="40px 60px">
-      {children()}
-    </Div>
-    <Div position="fixed" top="0" right="0" bottom="0" width="380px">
+    <ContentWrapper>{children()}</ContentWrapper>
+    <SideBarWrapper>
       <SideBar />
-    </Div>
+    </SideBarWrapper>
   </Div>
 );
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func
-};
-
-export default TemplateWrapper;
+export default Layout;
